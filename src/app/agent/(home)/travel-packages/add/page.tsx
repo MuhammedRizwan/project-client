ea90a328b7992132f82b94dfe1406540dc70f4f6
rizwan <mhdrizwanpkd@gmail.com>
2024-10-07 17:55:09 +0530

    feat:Add Package

--- src/app/agent/(home)/travel-packages/add/page.tsx
+++ src/app/agent/(home)/travel-packages/add/page.tsx
@@ -1,18 +1,70 @@
 'use client'
 import PackageForm, { PackageFormValues } from "@/components/package/PackageForm";
+import { Agent } from "@/interfaces/agent";
+import { RootState } from "@/store/store";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 
 const AddPackagePage = () => {
-  const handleAddPackage = (data: PackageFormValues) => {
-    console.log("Package Added", data);
+  const router = useRouter();
+  const agent:Agent|null=useSelector((state:RootState)=>state.agent.agent)
+  const [categories, setCategories] = useState([]); 
+  const handleAddPackage = async (data: PackageFormValues) => {
+    try {
+      if(!agent){
+        toast.error("Please login to add package");
+        return;
+      }
+      const res = await axios.post("http://localhost:5000/agent/package/add",{agentId:agent._id, ...data},{
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if(res.status === 201) {
+        router.push("/agent/travel-packages");
+        toast.success("Package added successfully");
+      }else{
+        console.log(res.data);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.message || "Failed to add package";
+        toast.error(errorMessage);
+      } else {
+        toast.error("An unknown error occurred");
+      }
+    }
   };
+  useEffect(() => {
+    const fetchCategories=async()=>{
+      try{
+        const res=await axios.get("http://localhost:5000/agent/category")
+        const {categories}=res.data
+        setCategories(categories)
+      }catch(error){
+        if (axios.isAxiosError(error)) {
+          const errorMessage =
+            error.response?.data?.message || "Failed to add category";
+          toast.error(errorMessage);
+        } else {
+          toast.error("An unknown error occurred");
+        }
+      }
+    }
+    fetchCategories()
+  },[])
 
   return (
     <div>
       <h1>Add New Package</h1>
       <PackageForm 
         initialData={undefined}
-        categories={[]}
+        categories={categories}
         onSubmit={handleAddPackage}
         formTitle="Add Package"
       />
--- src/components/package/PackageForm.tsx
+++ src/components/package/PackageForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Category } from "@/interfaces/category";
 import {
   Button,
   Dropdown,
@@ -13,11 +14,11 @@ import { useForm, useFieldArray } from "react-hook-form";
 
 export interface PackageFormValues {
   packageName: string;
-  category: string;
+  category: Category[];
   maxPerson: number;
   noOfDays: number;
   noOfNights: number;
-  destination: string;
+  destinations: string[];
   price: number;
   itineraries: {
     day: number;
@@ -28,7 +29,7 @@ export interface PackageFormValues {
 
 interface PackageFormProps {
   initialData?: PackageFormValues;
-  categories: string[];
+  categories: Category[];
   onSubmit: (data: PackageFormValues) => void;
   formTitle: string;
 }
@@ -50,11 +51,11 @@ export default function PackageForm({
   } = useForm<PackageFormValues>({
     defaultValues: {
       itineraries: [{ day: 1, activities: [{ time: "", activity: "" }] }],
+      destinations: [""],
       images: [],
       ...initialData,
     },
   });
-
   const [images, setImages] = useState<string[]>(initialData?.images || []);
   const { fields, append, remove } = useFieldArray({
     control,
@@ -75,18 +76,18 @@ export default function PackageForm({
     [selectedKeys]
   );
 
-
   const handleSelectionChange = (keys: unknown) => {
-    const selectedKeysAsArray = Array.from(keys as Set<string>); 
-    setSelectedKeys(new Set(selectedKeysAsArray)); 
-    setValue("category", selectedKeysAsArray.join(", ")); 
+    console.log(keys);
+    
+    const selectedCategory = categories.find((category) => category._id === keys);
+    setSelectedKeys(new Set([selectedCategory?.category_name ?? '']));
+    setValue("category", selectedCategory ? [selectedCategory] : []);
   };
 
   const handleDaysChange = (value: number) => {
     const currentItineraries = getValues("itineraries");
-  
-    if (value > currentItineraries.length) {
 
+    if (value > currentItineraries.length) {
       for (let i = currentItineraries.length; i < value; i++) {
         append({ day: i + 1, activities: [{ time: "", activity: "" }] });
       }
@@ -96,7 +97,30 @@ export default function PackageForm({
       }
     }
   };
-  
+
+  const [destinations, setDestinations] = useState<string[]>(
+    getValues("destinations") || []
+  );
+
+  // Function to add a new destination
+  const handleAddDestination = () => {
+    setDestinations([...destinations, ""]);
+  };
+
+  // Function to remove a destination
+  const handleRemoveDestination = (index: number) => {
+    const updatedDestinations = destinations.filter((_, i) => i !== index);
+    setDestinations(updatedDestinations);
+    setValue("destinations", updatedDestinations); // Update the form state
+  };
+
+  // Function to update a destination in the array
+  const handleChangeDestination = (index: number, value: string) => {
+    const updatedDestinations = [...destinations];
+    updatedDestinations[index] = value;
+    setDestinations(updatedDestinations);
+    setValue("destinations", updatedDestinations); // Update the form state
+  };
 
   const handleAddActivity = (dayIndex: number) => {
     const currentItineraries = getValues("itineraries");
@@ -211,27 +235,36 @@ export default function PackageForm({
                 disallowEmptySelection
                 selectionMode="single"
                 selectedKeys={selectedKeys}
-                onSelectionChange={handleSelectionChange}
+                onSelectionChange={(keys) => {
+                  const selectedCategory = Array.from(keys).join("");
+                  handleSelectionChange(selectedCategory);
+                }}
                 className="w-full"
               >
-                {categories.map((category) => (
-                  <DropdownItem key={category}>{category}</DropdownItem>
+                {categories.map((category: Category) => (
+                  <DropdownItem key={category._id}>
+                    {category.category_name}
+                  </DropdownItem>
                 ))}
               </DropdownMenu>
             </Dropdown>
+
+          
             <input
               type="hidden"
+              value={selectedValue} // Pass the selected value here
               {...register("category", {
-                required: "Category is required", // Add validation
+                required: "Category is required",
               })}
             />
+
+            {/* Validation error display */}
             <p className="text-red-500 text-xs min-h-[20px]">
               {errors.category?.message || ""}
             </p>
           </div>
         </div>
 
-        {/* Other Form Inputs */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="mb-4">
             <label
@@ -275,8 +308,10 @@ export default function PackageForm({
               {...register("noOfDays", {
                 required: "Number of days is required",
                 min: { value: 1, message: "Number of days must be at least 1" },
-                max: { value: 10, message: "Number of days must be at most 10" },
-               
+                max: {
+                  value: 10,
+                  message: "Number of days must be at most 10",
+                },
               })}
               placeholder="Number Of Days"
               onChange={(e) => handleDaysChange(Number(e.target.value))}
@@ -298,16 +333,22 @@ export default function PackageForm({
               type="number"
               {...register("noOfNights", {
                 required: "Number of nights is required",
-                min: { value: 0, message: "Number of nights must be at least 0" },
-                max: { value: 10, message: "Number of nights must be at most 10" },
+                min: {
+                  value: 0,
+                  message: "Number of nights must be at least 0",
+                },
+                max: {
+                  value: 10,
+                  message: "Number of nights must be at most 10",
+                },
                 validate: {
                   adjustDays: (nights) => {
-                    const days = getValues("noOfDays");                    
-                    if ((nights - days) > 1 || (nights - days) < -1  ) {
+                    const days = getValues("noOfDays");
+                    if (nights - days > 1 || nights - days < -1) {
                       return "Days must be either Nights - 1 or Nights + 1.";
                     }
                     return true;
-                  },                                                                                                                                                                                                                                                       
+                  },
                 },
               })}
               placeholder="Number Of Nights"
@@ -318,100 +359,186 @@ export default function PackageForm({
           </div>
         </div>
 
-        {/* Itineraries Section */}
-        <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg shadow-xl mt-8">
-          <h1 className="text-2xl font-bold mb-4">Itineraries</h1>
-          <p className="text-gray-600 mb-4">Add activities for each day.</p>
-
-          {fields.map((field, dayIndex) => (
-            <div key={field.id} className="mb-6">
-              <h2 className="text-lg font-semibold mb-4">Day {dayIndex + 1}</h2>
-
-              {field.activities.map((activity, activityIndex) => (
-                <div key={activityIndex} className="flex space-x-4 mb-4">
-                  <Input
-                    type="time"
-                    {...register(
-                      `itineraries.${dayIndex}.activities.${activityIndex}.time` as const
-                    )}
-                    placeholder="Activity Time"
-                  />
-                  <Input
-                    {...register(
-                      `itineraries.${dayIndex}.activities.${activityIndex}.activity` as const
-                    )}
-                    placeholder="Activity Description"
-                  />
-                  <Button
-                    type="button"
-                    color="danger"
-                    onClick={() =>
-                      handleRemoveActivity(dayIndex, activityIndex)
-                    }
-                  >
-                    Remove Activity
-                  </Button>
-                </div>
-              ))}
-
-              <Button
-                type="button"
-                color="primary"
-                onClick={() => handleAddActivity(dayIndex)}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="max-person"
+            >
+              Destination
+            </label>
+            {destinations.map((destination, index) => (
+              <div
+                key={index}
+                className="flex items-start mb-2 justify-between "
               >
-                Add Activity
-              </Button>
-            </div>
-          ))}
-        </div>
+                <div>
+                  <Input
+                    {...register(`destinations.${index}`, {
+                      required: "Destination is required",
+                      pattern: {
+                        value: /^[A-Za-z0-9 ]+$/,
+                        message:
+                          "No special characters allowed in destination name",
+                      },
+                    })}
+                    value={destination}
+                    placeholder="Destination"
+                    className="flex-grow w-96"
+                    onChange={(e) =>
+                      handleChangeDestination(index, e.target.value)
+                    }
+                  />
 
-        {/* Images Section */}
-        <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg shadow-xl mt-8">
-          <h1 className="text-2xl font-bold mb-4">Images</h1>
-          <p className="text-gray-600 mb-4">
-            Upload images related to the package.
-          </p>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {images.map((image, index) => (
-              <div key={index} className="relative">
-                <Image
-                  src={image}
-                  alt={`Uploaded image ${index + 1}`}
-                  width={300}
-                  height={200}
-                  className="rounded-md"
+                  <p className="text-red-500 text-xs min-h-[20px]">
+                    {errors.destinations?.[index]?.message}
+                  </p>
+                </div>
+                <Button
+                  type="button"
+                  onClick={() => handleRemoveDestination(index)}
+                  className="ml-2"
+                  color="danger"
+                  isIconOnly
+                >
+                  X
+                </Button>
+              </div>
+            ))}
+            <Button
+              type="button"
+              onClick={handleAddDestination}
+              color="primary"
+            >
+              + Add Destination
+            </Button>
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="no-of-nights"
+            >
+              Price
+            </label>
+            <Input
+              type="number"
+              {...register("price", {
+                required: "Price is required",
+                min: {
+                  value: 1,
+                  message: "Price must be at least 1",
+                },
+              })}
+              placeholder="Price of package"
+            />
+            <p className="text-red-500 text-xs min-h-[20px]">
+              {errors.price?.message || ""}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg shadow-xl mt-8">
+        <h1 className="text-2xl font-bold mb-4">Itineraries</h1>
+        <p className="text-gray-600 mb-4">Add activities for each day.</p>
+
+        {fields.map((field, dayIndex) => (
+          <div key={field.id} className="mb-6">
+            <h2 className="text-lg font-semibold mb-4">Day {dayIndex + 1}</h2>
+
+            {field.activities.map((activity, activityIndex) => (
+              <div key={activityIndex} className="flex space-x-4 mb-4">
+                <Input
+                  type="time"
+                  className="w-32" // Reduced size of the time input
+                  {...register(
+                    `itineraries.${dayIndex}.activities.${activityIndex}.time`
+                  )}
+                  placeholder="Activity Time"
+                />
+                {errors.itineraries?.[dayIndex]?.activities?.[activityIndex]
+                  ?.time && (
+                  <span className="text-red-500 text-sm">
+                    {
+                      errors.itineraries[dayIndex].activities[activityIndex]
+                        .time.message
+                    }
+                  </span>
+                )}
+                <Input
+                  {...register(
+                    `itineraries.${dayIndex}.activities.${activityIndex}.activity`
+                  )}
+                  placeholder="Activity Description"
                 />
                 <Button
                   type="button"
                   color="danger"
-                  onClick={() => handleDelete(index)}
-                  className="absolute top-0 right-0"
+                  onClick={() => handleRemoveActivity(dayIndex, activityIndex)}
                 >
-                  Remove
-                </Button>
-                <Button
-                  type="button"
-                  onClick={() => handleChange(index)}
-                  className="absolute top-0 left-0"
-                >
-                  Change
+                  Remove Activity
                 </Button>
               </div>
             ))}
-          </div>
-          <Input
-            type="file"
-            onChange={handleImageChange}
-            multiple
-            className="mt-4"
-          />
-        </div>
 
-        <div className=" mx-auto  p-8  mt-8 flex justify-end">
-          <Button type="submit" className="bg-yellow-700 hover:bg-yellow-800">
-            Submit Package
-          </Button>
+            <Button
+              type="button"
+              color="primary"
+              onClick={() => handleAddActivity(dayIndex)}
+              className="mt-4"
+            >
+              Add Activity
+            </Button>
+          </div>
+        ))}
+      </div>
+
+      <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg shadow-xl mt-8">
+        <h1 className="text-2xl font-bold mb-4">Images</h1>
+        <p className="text-gray-600 mb-4">
+          Upload images related to the package.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {images.map((image, index) => (
+            <div key={index} className="relative">
+              <Image
+                src={image}
+                alt={`Uploaded image ${index + 1}`}
+                width={300}
+                height={200}
+                className="rounded-md"
+              />
+              <Button
+                type="button"
+                color="danger"
+                onClick={() => handleDelete(index)}
+                className="absolute top-0 right-0"
+              >
+                Remove
+              </Button>
+              <Button
+                type="button"
+                onClick={() => handleChange(index)}
+                className="absolute top-0 left-0"
+              >
+                Change
+              </Button>
+            </div>
+          ))}
         </div>
+        <Input
+          type="file"
+          onChange={handleImageChange}
+          multiple
+          className="mt-4"
+        />
+      </div>
+
+      <div className=" mx-auto  p-8  mt-8 flex justify-end">
+        <Button type="submit" className="bg-yellow-700 hover:bg-yellow-800">
+          Submit Package
+        </Button>
       </div>
     </form>
   );
