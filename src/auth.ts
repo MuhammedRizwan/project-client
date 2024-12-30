0648effa6c033537698e36fde1b9f908f4286b71
rizwan <mhdrizwanpkd@gmail.com>
2024-12-30 11:07:20 +0530

    google configuration updated

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,11 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
   secret: process.env.NEXTAUTH_SECRET,
 })
