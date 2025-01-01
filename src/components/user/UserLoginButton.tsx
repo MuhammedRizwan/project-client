ca1c559e0403e4e19176a6a5f788b2a6bbe4e34e
rizwan <mhdrizwanpkd@gmail.com>
2025-01-01 21:55:50 +0530

    logout button added

--- src/components/user/UserLoginButton.tsx
+++ src/components/user/UserLoginButton.tsx
@@ -7,12 +7,12 @@ import {
   DropdownTrigger,
   User,
 } from "@nextui-org/react";
-import { signOut as nextAuthSignOut } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "@/store/reducer/userReducer";
 import Cookies from "js-cookie";
 import { RootState } from "@/store/store";
+import { googleLogout } from "@react-oauth/google";
 
 export default function UserLoginButton() {
   const router = useRouter();
@@ -20,50 +20,85 @@ export default function UserLoginButton() {
   const pathname = usePathname();
   const accessToken = Cookies.get("accessToken");
   const dispatch = useDispatch();
-  const handleLogout = async () => {
+  const handleLogout = () => {
     dispatch(logout());
     if (user?.google_authenticated) {
-      await nextAuthSignOut({ callbackUrl: "/login" });
-    } else {
-      router.push("/login");
+      googleLogout();
     }
+    router.push("/login");
   };
   return (
     <>
       {accessToken || user ? (
         <Dropdown placement="bottom-start">
           <DropdownTrigger>
-           {user?.profile_picture && typeof user?.profile_picture === "string" ?(
+            {user?.profile_picture &&
+            typeof user?.profile_picture === "string" ? (
               <User
-              as="button"
-              avatarProps={{
-                isBordered: true,
-                src:  user?.profile_picture ,
-              }}
-              className="transition-transform"
-              description={user?.email || ""}
-              name={user?.username || ""}
-            />
-           ):(
-            <User
-            as="button"
-            avatarProps={{
-              isBordered: true,
-              src: "" ,
-            }}
-            className="transition-transform"
-            description={user?.email || ""}
-            name={user?.username || ""}
-          />
-           )}
+                as="button"
+                avatarProps={{
+                  isBordered: true,
+                  src: user?.profile_picture,
+                }}
+                className="transition-transform"
+                description={user?.email || ""}
+                name={user?.username || ""}
+              />
+            ) : (
+              <User
+                as="button"
+                avatarProps={{
+                  isBordered: true,
+                  src: "",
+                }}
+                className="transition-transform"
+                description={user?.email || ""}
+                name={user?.username || ""}
+              />
+            )}
           </DropdownTrigger>
           <DropdownMenu aria-label="User Actions" variant="flat">
-            <DropdownItem key="profile" color="warning" onClick={()=>router.push("/profile-edit")}>profile</DropdownItem>
-            <DropdownItem key="travel history" color="warning" onClick={()=>router.push("/travel-history")}>travel history</DropdownItem>
-            <DropdownItem key="bookings" color="warning" onClick={()=>router.push("/booked")}>my bookings</DropdownItem>
-            <DropdownItem key="system" onClick={()=>router.push("/notification")}>notification</DropdownItem>
-            <DropdownItem key="Chats" color="warning" onClick={()=>router.push("/chat")}>friends</DropdownItem>
-            <DropdownItem key="my posts" color="warning" onClick={()=>router.push("/user-post")}>my posts</DropdownItem>
+            <DropdownItem
+              key="profile"
+              color="warning"
+              onClick={() => router.push("/profile-edit")}
+            >
+              profile
+            </DropdownItem>
+            <DropdownItem
+              key="travel history"
+              color="warning"
+              onClick={() => router.push("/travel-history")}
+            >
+              travel history
+            </DropdownItem>
+            <DropdownItem
+              key="bookings"
+              color="warning"
+              onClick={() => router.push("/booked")}
+            >
+              my bookings
+            </DropdownItem>
+            <DropdownItem
+              key="system"
+              onClick={() => router.push("/notification")}
+            >
+              notification
+            </DropdownItem>
+            <DropdownItem
+              key="Chats"
+              color="warning"
+              onClick={() => router.push("/chat")}
+            >
+              friends
+            </DropdownItem>
+            <DropdownItem
+              key="my posts"
+              color="warning"
+              onClick={() => router.push("/user-post")}
+            >
+              my posts
+            </DropdownItem>
             <DropdownItem key="logout" color="danger" onClick={handleLogout}>
               Logout
             </DropdownItem>
