98eb675b281d3fee6744c8c991f053ac7e0aa8af
rizwan <mhdrizwanpkd@gmail.com>
2025-01-01 18:47:11 +0530

    middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/auth"   
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-forwarded-proto', 'https')
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
